fb2496c127798ba21e9ed2cffaa3a3ec8da00107
Joyqul Chen <joy1357924680@gmail.com>
2016-01-13 02:18:17 +0800

    change home page design

--- final/js/main.js
+++ final/js/main.js
@@ -1,49 +1,14 @@
 $(document).ready(function() {
     re_win();
-    init();
 	$(window).resize(function() {
         re_win();
 	});
-    $(".pics").cycle({
-        fx: 'fade'
+    $(".flexslider").flexslider({
+        animation:"slide"
     });
 });
-
-function init() {
-    var w = $(window).innerWidth();
-    var h = $(window).innerHeight();
-    if (w <= 768) {
-        var images = $("img");
-        images.each(function(i) {
-            var origin = $(this).attr("src");
-            var modify = origin.replace(".", "@small.");
-            $(this).attr("src", modify);
-        });
-        $("img").css({
-            height: h
-        });
-    }
-}
 
 function re_win() {
     var w = $(window).innerWidth();
     var h = $(window).innerHeight();
-    $(".black").width(w);
-    $(".black").height(h);
-    $(".title").css({
-        top: Math.floor(h/2)-60
-    });
-    if (w > 768) {
-        $(".title").css({
-            left: Math.floor(w/8)
-        });
-    }
-    else {
-        $(".title").css({
-            left: 20
-        });
-    }
-    $(".page").css({
-        height: h
-    });
 }
